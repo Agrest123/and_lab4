7b82495851f4fd3c72f7713314baf5d89471bae6
Bartosz Wilczanski <b.wilczanski@doittechnology.pl>
2019-04-13 16:18:07 +0200

    lab5 zad2

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_lab4;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +9,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,8 +17,10 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+//    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
 
+    MySQLite db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +34,23 @@ public class MainActivity extends AppCompatActivity {
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
 
-        this.adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,this.target);
+//        this.adapter = new ArrayAdapter(this,
+//                android.R.layout.simple_list_item_1,this.target);
 
-        ListView listview = (ListView) findViewById(
-                R.id.listView );
+
+        this.adapter = new SimpleCursorAdapter(
+                this, android.R.layout.simple_list_item_2,
+                db.lista(),
+                new String[] {"_id", "gatunek"},
+                new int[] {android.R.id.text1,
+                        android.R.id.text2},
+
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+        );
+
+        ListView listview = (ListView) findViewById( R.id.listView );
         listview.setAdapter(this.adapter);
+
 
     }
 
