9a78eeb9014df12bbd91c472ff30ffdb7ad16dbd
Bartosz Wilczanski <b.wilczanski@doittechnology.pl>
2019-04-13 18:05:31 +0200

    lab5 zad4

--- app/src/main/java/com/example/android_lab4/DodajWpis.java
+++ app/src/main/java/com/example/android_lab4/DodajWpis.java
@@ -10,6 +10,8 @@ import android.widget.Spinner;
 
 public class DodajWpis extends AppCompatActivity {
 
+    private int modyfi_id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +19,26 @@ public class DodajWpis extends AppCompatActivity {
         ArrayAdapter gatunki = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, new String[] {"Pies", "Kot", "Rybki"});
         Spinner gatunek = (Spinner) findViewById(R.id.gatunek);
         gatunek.setAdapter(gatunki);
-   }
+
+
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            if(extras.getSerializable("element") != null) {
+                Animal zwierz = (Animal) extras.getSerializable("element");
+                EditText kolor = (EditText) findViewById(R.id.kolor);
+                EditText wielkość = (EditText) findViewById(R.id.wielkosc);
+                EditText opis = (EditText) findViewById(R.id.opis);
+                kolor.setText(zwierz.getKolor());
+                wielkość.setText(Float.toString(zwierz.getWielkosc()));
+                opis.setText(zwierz.getOpis());
+                this.modyfi_id=zwierz.getId();
+            }
+        }catch(Exception ex) {
+            this.modyfi_id=0;
+        }
+
+    }
 
     public void wyslij (View view)
     {
@@ -31,6 +52,9 @@ public class DodajWpis extends AppCompatActivity {
                 Float.valueOf(wielkosc.getText().toString()),
                 opis.getText().toString()
         );
+
+        zwierze.setId(this.modyfi_id);
+
         Intent intencja = new Intent();
         intencja.putExtra("nowy", zwierze);
         setResult(RESULT_OK, intencja);
